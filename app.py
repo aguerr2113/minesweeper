812d67aed030df06640973100efc6d9af5418ca3
aguerrero2113 <aguerrero2113@github.com>
2023-02-12 21:08:31 -0600

    added some events to right and left click on buttons.added a grid using for loop and a cell class

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from tkinter import *
+from cell import Cell
 import settings
 import utilities
+
 
 
 root = Tk()
@@ -17,7 +19,7 @@ root.resizable(False,False)
 
 top_frame = Frame(
     root,
-    bg = 'red',
+    bg = 'black',
     width = settings.width,
     height = utilities.height_prct(25)
 )
@@ -25,11 +27,48 @@ top_frame.place(x=0,y=0)
 
 left_frame = Frame(
     root,
-    bg='blue',
+    bg='black',
     width = utilities.width_prct(25),
     height= utilities.height_prct(75) #subtract full height of window720 from the previous heigth of top_frame to get this height
 )
 left_frame.place(x=0,y=utilities.height_prct(25))
+
+center_frame = Frame(
+    root,
+    bg = 'black',
+    width = utilities.width_prct(75),
+    height = utilities.height_prct(75)
+)
+
+center_frame.place(
+    x = utilities.width_prct(25),
+    y = utilities.height_prct(25)
+)
+
+
+for x in range(settings.grid_size):
+    for y in range(settings.grid_size):
+        c = Cell()
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column = x,
+            row = y
+        )
+
+# c1 = Cell()
+# c1.create_btn_object(center_frame)
+# c1.cell_btn_object.grid(
+#     column = 0,
+#     row = 0
+# )
+# c2 = Cell()
+# c2.create_btn_object(center_frame)
+# c2.cell_btn_object.grid(
+#     column = 0,
+#     row = 1
+# )
+
+
 
 # Run the Window
 root.mainloop()
